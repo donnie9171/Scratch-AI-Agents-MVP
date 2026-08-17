cd8832f326c52cfb4e8c20cca18188e7b8e12e09
Enoch Kang <enochkang9171@gmail.com>
2026-08-16 23:07:06 -0400

    update azure config

--- helper/azureConfig.js
+++ helper/azureConfig.js
@@ -1,4 +1,4 @@
 // azureConfig.js
-const AZURE_SOURCE = "https://multiagent-exploration-workbench-c8htdxerg6d9faa0.uksouth-01.azurewebsites.net"; // or "http://localhost:7071"
+const AZURE_SOURCE = "https://mewapiproxy-f7hvb2fufphra5bm.uaenorth-01.azurewebsites.net"; // or "http://localhost:7071"
 
 export { AZURE_SOURCE };
